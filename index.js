6539657b53e3945b93c448bbf608825cff6d544d
Daniel Rucci <drucci@hubspot.com>
2017-09-28 13:50:38 -0400

    lskdfjskljdf

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ var bodyParser = require('body-parser')
 var app = express()
 
 var magnitude = 0;
-var direction = 0;
+var angle = 0;
 
-var count = 100;
-
+var count = 0;
 var magX = 0;
 var magY = 0;
 
+
+var seen = new Set()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.raw())
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!' + magnitude + " direction: "+ direction);
+  response.send('Hello World!' + magnitude + " direction: "+ angle);
 })
 
 app.get('/current', function(request, response) {
@@ -25,28 +26,35 @@ app.get('/current', function(request, response) {
 })
 
 function getCurrent() {
-	return {"direction": direction, magnitude: magnitude}
+	return {"direction": angle, magnitude: magnitude}
 }
 
-app.post('/', function(request, response) {
+app.get('/send', function(request, response) {
 	console.log(request)
 
-	newMagX =  (request.query.magnitude * Math.cos(request.query.direction)) / count;
+	ident = request.query.clientId
+	if (!seen.has(ident)) {
+		seen.add(ident)
+		count += count
+		console.log("NEW IDENTFIER, INCREASING COUNT")
+	}
+
+	newMagX =  (request.query.magnitude * Math.cos(request.query.angle)) / count;
 	magX += newMagX
 	oldMagX =  (request.query.oldMag * Math.cos(request.query.oldDirection)) / count;
 	magX -= oldMagX
 
-	newMagY =  (request.query.magnitude * Math.sin(request.query.direction)) / count;
+	newMagY =  (request.query.magnitude * Math.sin(request.query.angle)) / count;
 	magY += newMagY
 	oldMagY =  (request.query.oldMag * Math.sin(request.query.oldDirection)) / count;
 	magY -= oldMagY
 
 
 	magnitude = Math.sqrt(Math.pow(magX,2) + Math.pow(magY,2))
-	direction = Math.atan(magY/magX)
+	angle = Math.atan(magY/magX)
 
 	console.log("inputed magnitude " + request.query.magnitude)
-	console.log("inputed direction " + request.query.direction)
+	console.log("inputed direction " + request.query.angle)
 	console.log("inputed magnitude " + request.query.magnitude)
 	console.log("new direction " + direction)
 	console.log("new magnitude " + magnitude)
